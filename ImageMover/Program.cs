d98157ed0bab8b850e6ef5d0c3ee74f470faf300
Lewis Zou <zlzforever@163.com>
2022-10-14 22:30:57 +0800

    let's rock

---
+++ ImageMover/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// args = new string[] { "/Users/lewis/Downloads/xianju/18" };
+
+var path = args.ElementAtOrDefault(0);
+if (string.IsNullOrWhiteSpace(path))
+{
+    Console.WriteLine("前输入图片根目录");
+    return;
+}
+
+var di = new DirectoryInfo(path);
+if (!di.Exists)
+{
+    Console.WriteLine("图片根目录不存在");
+    return;
+}
+
+
+var directories = Directory.GetDirectories(path);
+
+foreach (var directory in directories)
+{
+    var interval = Path.GetRelativePath(path, directory);
+    var files = Directory.GetFiles(directory);
+    foreach (var file in files)
+    {
+        var name = Path.GetFileName(file);
+        var newFileName = $"{di.Name}_{interval}_{name}";
+        var targetPath = Path.Combine(path, newFileName);
+        if (!File.Exists(targetPath))
+        {
+            File.Copy(file, targetPath);
+        }
+
+        Console.WriteLine($"{newFileName} moved");
+    }
+}
+
+Console.WriteLine("Bye!");
